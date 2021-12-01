f0b424cb2b58a7a435f2182f45c21c2015e4cbeb
Igor Korovchenko <igsekor@gmail.com>
2021-12-01 19:07:42 +0300

    Add frontmatter

---
+++ src/modules/frontmatter.js
@@ -0,0 +1,70 @@
+import { BaseModule } from './base.js'
+import { filter } from "minimatch"
+import fm from 'front-matter'
+import fs from 'fs'
+
+export class FrontmatterModule extends BaseModule {
+  MODULE_KEY = 'frontmatter'
+
+  isApplicable(label, caseNumber = null) {
+    let config
+    if (caseNumber === null) {
+      config = this.config[label].meta
+    } else {
+      config = this.config[label][caseNumber].meta
+    }
+
+    const files = this.getMarkdownFiles(this.objects, label)
+    if (Array.isArray(files)) {
+      files.forEach(f => {
+        const meta = this.getFrontmatterObject(f)
+        for (const key in config) {
+          if (Object.hasOwnProperty.call(config, key)) {
+            if (meta.hasOwnProperty(key) && this.isApplicableValuesForKey(config[key], meta[key])) {
+              return true
+            }
+          }
+        }
+      })
+      return false
+    }
+
+    return undefined
+  }
+
+  getMarkdownFiles(fileObjects, label) {
+    const fileList = []
+    console.log(`Module — ${MODULE_KEY}, label - ${label}:`)
+    for (const index in fileObjects.data) {
+      const file = fileObjects.data[index]
+      if (typeof file === 'object' && file.status && file.filename) {
+        fileList.push(file.filename)
+      }
+    }
+    return fileList.filter(filter('*.md'))
+  }
+
+  getFrontmatterObject(filename) {
+    try {
+      const content = fs.readFileSync(filename, { encoding: 'utf8' })
+      return fm(content)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  isApplicableValuesForKey(configValues, metaValues) {
+    if (Array.isArray(configValues)) {
+      configValues.forEach(v => {
+        if (metaValues.includes(v)) {
+          return true
+        }
+      })
+    } else if (typeof configValues === 'object' && typeof metaValues === 'object') {
+      return JSON.stringify(configValues) === JSON.stringify(metaValues)
+    } else {
+      return configValues === metaValues
+    }
+    return false
+  }
+}
