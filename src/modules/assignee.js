a347be9b9e1479094b8d517b8b9772f2addf5e3e
Igor Korovchenko <igsekor@gmail.com>
2021-12-01 16:48:16 +0300

    Add assignee module

---
+++ src/modules/assignee.js
@@ -0,0 +1,27 @@
+import { BaseModule } from "./base.js"
+
+export class AssigneeModule extends BaseModule {
+  MODULE_KEY = 'assignee'
+
+  isApplicable(label) {
+    const assignee = this.config[label].assignee
+
+    if (Array.isArray(assignee) && Array.isArray(this.objects)) {
+      assignee.forEach((a) => {
+        if (this.objects.includes(a)) {
+          console.log(`Module — ${MODULE_KEY}, label - ${label}: has applicable assignee`)
+          return true
+        }
+      })
+    } else if (typeof assignee === 'string' && Array.isArray(this.objects)) {
+      if (this.objects.includes(assignee)) {
+        console.log(`Module — ${MODULE_KEY}, label - ${label}: has applicable assignee`)
+        return true
+      }
+    } else {
+      return undefined
+    }
+
+    return false
+  }
+}
