01415e336ffc97408b40c71c2c0d86be9f75692c
Igor Korovchenko <igsekor@gmail.com>
2021-12-06 19:43:46 +0300

    fix all issues

--- src/modules/files.js
+++ src/modules/files.js
@@ -1,5 +1,5 @@
 import { BaseModule } from "./base.js"
-import createMatcher from 'picomatch';
+import picomatch from 'picomatch'
 
 const FILE_STATUSES = [
   'added',
@@ -12,55 +12,72 @@ export class FilesModule extends BaseModule {
   MODULE_KEY = 'files'
 
   isApplicable(label, caseNumber = null) {
-    return this.hasApplicableFiles(this.objects, label, caseNumber)
+    if (caseNumber === 0) {
+      return this.hasApplicableFiles(this.objects, label, 0)
+    }
+    return this.hasApplicableFiles(this.objects, label, caseNumber || null)
   }
 
   hasApplicableFiles(fileObjects, label, caseNumber) {
+    if (typeof this.config[label] !== 'object') {
+      return undefined
+    }
     let config = null
     if (caseNumber === null) {
-      config = this.config[label].files
+      if (this.config[label].hasOwnProperty('files')) {
+        config = this.config[label].files
+      } else {
+        return undefined
+      }
     } else {
-      config = this.config[label][caseNumber].files
+      if (this.config[label].hasOwnProperty(caseNumber)
+        && this.config[label][caseNumber].hasOwnProperty('files')) {
+        config = this.config[label][caseNumber].files
+      } else {
+        return undefined
+      }
     }
 
     if (typeof config === 'string') {
       return this.areFilesApplicable(fileObjects, label, [config])
     } else if (Array.isArray(config)) {
-      return this.areFilesApplicable(fileObjects, label, config)
-    } else if (typeof config === 'object') {
-      const patterns = Object.keys(config)
-      patterns.forEach(s => {
-        if (this.areFilesApplicable(fileObjects, label, patterns[s])) {
-          return true
-        }
-      })
+      return this.areFilesApplicable(fileObjects, label, [...config])
+    } else {
+      return undefined
     }
-
-    return undefined
   }
 
   areFilesApplicable(fileObjects, label, patterns) {
-    if (patterns && patterns.length > 0) {
-      const fileList = []
-      console.log(`Module — ${MODULE_KEY}, label - ${label}:`)
-      for (const index in fileObjects.data) {
-        const file = fileObjects.data[index]
-        if (typeof file === 'object' && file.status && file.filename) {
-          fileList.push(file.filename)
-        }
+    if (Array.isArray(patterns)) {
+      let fileList = this.getArrayFromGitHubFiles(fileObjects)
+      if (Array.isArray(fileList) && fileList.length > 0) {
+        console.log(`Module — ${this.MODULE_KEY}, label - ${label}:`)
+      } else {
+        return false
       }
 
-      patterns.forEach(p => {
-        fileList.filter(createMatcher(p))
-      })
-      fileList.forEach(f => {
-        console.log(`File ${f} is taken into account`)
-      })
-
-      const selectedFilesCount = fileList.length
-      console.log(`---\n${selectedFilesCount} files were applicable`)
-      return selectedFilesCount > 0
+      for (let i = 0; i < patterns.length; i++) {
+        const isMatch = picomatch(patterns[i])
+        const filteredList = fileList.filter(p => isMatch(p))
+        if (filteredList.length > 0) {
+          filteredList.forEach(f => {
+            console.log(`File "${f}" is taken into account`)
+          })
+          return true
+        }
+      }
     }
     return false
+  }
+
+  getArrayFromGitHubFiles(fileObjects) {
+    const fileList = []
+    for (const i in fileObjects) {
+      const file = fileObjects[i]
+      if (typeof file === 'object' && file.status && file.filename) {
+        fileList.push(file.filename)
+      }
+    }
+    return fileList
   }
 }
