0dce1bc95d476c71a8ec6852a3a709cbd138f777
Igor Korovchenko <igsekor@gmail.com>
2021-12-06 17:31:43 +0300

    add tests for array case

--- src/modules/assignee.test.js
+++ src/modules/assignee.test.js
@@ -203,3 +203,94 @@ test('AssigneeModule method isApplicable returns correct values for array', t =>
   t.is(module2.isApplicable('label2'), true)
   t.is(module2.isApplicable('label3'), false)
 })
+
+test('AssigneeModule method isApplicable returns correct values for mix values in array case', t => {
+  const objects = [
+    {
+      "login": "user1",
+      "id": 1,
+      "node_id": "MDQ6VXNlcjE=",
+      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/octocat",
+      "html_url": "https://github.com/octocat",
+      "followers_url": "https://api.github.com/users/octocat/followers",
+      "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+      "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+      "organizations_url": "https://api.github.com/users/octocat/orgs",
+      "repos_url": "https://api.github.com/users/octocat/repos",
+      "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/octocat/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    {
+      "login": "user2",
+      "id": 1,
+      "node_id": "MDQ6VXNlcjE=",
+      "avatar_url": "https://github.com/images/error/hubot_happy.gif",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/hubot",
+      "html_url": "https://github.com/hubot",
+      "followers_url": "https://api.github.com/users/hubot/followers",
+      "following_url": "https://api.github.com/users/hubot/following{/other_user}",
+      "gists_url": "https://api.github.com/users/hubot/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/hubot/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/hubot/subscriptions",
+      "organizations_url": "https://api.github.com/users/hubot/orgs",
+      "repos_url": "https://api.github.com/users/hubot/repos",
+      "events_url": "https://api.github.com/users/hubot/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/hubot/received_events",
+      "type": "User",
+      "site_admin": true
+    }
+  ]
+  const config3 = {
+    label1: [
+      {
+        assignee: [
+          'user1',
+          'user3'
+        ],
+        meta: {
+          field: 'example'
+        }
+      },
+      {
+        files: [
+          '*.md'
+        ]
+      },
+      {
+        meta: {
+          field: [
+            'example 1',
+            'example 2'
+          ]
+        }
+      }
+    ],
+    label2: {
+      assignee: [
+        'user2',
+        'user3'
+      ]
+    },
+    label3: {
+      assignee: 'user1'
+    },
+    label4: {
+      assignee: 'user3'
+    }
+  }
+
+  const module3 = new AssigneeModule(objects, config3)
+
+  t.is(module3.isApplicable('label1', 0), true)
+  t.is(module3.isApplicable('label1', 1), undefined)
+  t.is(module3.isApplicable('label2'), true)
+  t.is(module3.isApplicable('label3'), true)
+  t.is(module3.isApplicable('label4'), false)
+})
