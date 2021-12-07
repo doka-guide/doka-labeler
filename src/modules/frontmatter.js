2c1da70853277ef8a01b6f422883ea5c4a7b1afb
Igor Korovchenko <igsekor@gmail.com>
2021-12-07 21:40:34 +0300

    fix many bugs for the module

--- src/modules/frontmatter.js
+++ src/modules/frontmatter.js
@@ -1,5 +1,4 @@
 import { BaseModule } from './base.js'
-import createMatcher from 'picomatch';
 import fm from 'front-matter'
 import fs from 'fs'
 
@@ -7,25 +6,36 @@ export class FrontmatterModule extends BaseModule {
   MODULE_KEY = 'frontmatter'
 
   isApplicable(label, caseNumber = null) {
+    if (typeof this.config[label] !== 'object') {
+      return undefined
+    }
+
     let config
     if (caseNumber === null) {
-      config = this.config[label].meta
+      if (this.config[label].hasOwnProperty('meta')) {
+        config = this.config[label].meta
+      } else {
+        return undefined
+      }
     } else {
-      config = this.config[label][caseNumber].meta
+      if (this.config[label].hasOwnProperty(caseNumber)
+        && this.config[label][caseNumber].hasOwnProperty('meta')) {
+        config = this.config[label][caseNumber].meta
+      } else {
+        return undefined
+      }
     }
 
     const files = this.getMarkdownFiles(this.objects, label)
     if (Array.isArray(files)) {
-      files.forEach(f => {
+      for (let i = 0; i < files.length; i++) {
+        const f = files[i]
         const meta = this.getFrontmatterObject(f)
-        for (const key in config) {
-          if (Object.hasOwnProperty.call(config, key)) {
-            if (meta.hasOwnProperty(key) && this.isApplicableValuesForKey(config[key], meta[key])) {
-              return true
-            }
-          }
+        const result = this.isApplicableValuesForKey(config, meta)
+        if (typeof result === 'boolean' && result) {
+          return true
         }
-      })
+      }
       return false
     }
 
@@ -33,38 +43,72 @@ export class FrontmatterModule extends BaseModule {
   }
 
   getMarkdownFiles(fileObjects, label) {
-    const fileList = []
-    console.log(`Module — ${MODULE_KEY}, label - ${label}:`)
-    for (const index in fileObjects.data) {
-      const file = fileObjects.data[index]
+    let fileList = []
+    console.log(`Module — ${this.MODULE_KEY}, label - ${label}:`)
+    for (const index in fileObjects) {
+      const file = fileObjects[index]
       if (typeof file === 'object' && file.status && file.filename) {
         fileList.push(file.filename)
       }
     }
-    return fileList.filter(createMatcher('*.md'))
+    const mdRegEx = /.\.md$/i
+    fileList = fileList.filter(p => mdRegEx.test(p))
+    fileList.forEach(f => console.log(`File "${f}" is taken into account`))
+    return fileList
   }
 
   getFrontmatterObject(filename) {
     try {
       const content = fs.readFileSync(filename, { encoding: 'utf8' })
-      return fm(content)
+      return fm(content).attributes
     } catch (err) {
       console.error(err)
     }
   }
 
   isApplicableValuesForKey(configValues, metaValues) {
-    if (Array.isArray(configValues)) {
-      configValues.forEach(v => {
-        if (metaValues.includes(v)) {
+    if (Array.isArray(configValues) ) {
+      for (let i = 0; i < configValues.length; i++) {
+        if (metaValues.hasOwnProperty(configValues[i])) {
           return true
         }
-      })
-    } else if (typeof configValues === 'object' && typeof metaValues === 'object') {
-      return JSON.stringify(configValues) === JSON.stringify(metaValues)
+      }
+    } else if (typeof configValues === 'object') {
+      for (const key in configValues) {
+        if (configValues.hasOwnProperty(key)) {
+          const valuesForKey = configValues[key]
+          if (Array.isArray(valuesForKey)) {
+            for (let i = 0; i < valuesForKey.length; i++) {
+              const v = valuesForKey[i]
+              if (Array.isArray(metaValues[key])) {
+                if (metaValues[key].includes(v)) {
+                  return true
+                }
+              } else if (typeof metaValues[key] === 'string') {
+                if (metaValues[key] === v) {
+                  return true
+                }
+              }
+            }
+          } else if (typeof valuesForKey === 'string') {
+            if (Array.isArray(metaValues[key])) {
+              if (metaValues[key].includes(valuesForKey)) {
+                return true
+              }
+            } else if (typeof metaValues[key] === 'string') {
+              if (metaValues[key] === valuesForKey) {
+                return true
+              }
+            }
+          } else {
+            return false
+          }
+        }
+      }
+    } else if (typeof configValues === 'string') {
+      return metaValues.hasOwnProperty(configValues)
     } else {
-      return configValues === metaValues
+      return false
     }
-    return false
   }
 }
